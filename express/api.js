b61e8e6ed7d1c8c8ed2754e2e81650335a55fef5
Thaciane de Paula Fernandes <info2020.20203029968@gmail.com>
2025-09-30 11:58:43 -0300

    Update api.js

--- express/api.js
+++ express/api.js
@@ -3,8 +3,8 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 const arrResponse = [
-    { name: "Thaciane", company: "Testando API" },
-    { name: "Thaciane", company: "Meu objetivo é conseguir estágio em desenvolvimento de sistemas"},
+    { name: "Thaciane", company: "Estágio Full Stack" },
+    { name: "Thaciane", company: "Estágio em desenvolvimento de sistemas"},
 ];
 
 app.get("/", (req, res) =>{
